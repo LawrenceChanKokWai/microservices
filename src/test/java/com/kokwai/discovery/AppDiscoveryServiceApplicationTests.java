497193c6e9f4b26f295f6d3aceb03a94dd387633
LawrenceChanKokWai <kokwai2107@gmail.com>
2024-11-05 10:12:18 +0800

    initial commit

---
+++ src/test/java/com/kokwai/discovery/AppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kokwai.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
